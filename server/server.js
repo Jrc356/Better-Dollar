58f9292da849bf72a8212fc5bd050d60b31cd268
Jon Corbin <jon.corbin8@gmail.com>
2020-01-20 14:59:36 -0500

    Convert to nextjs app

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 
+// Imports
 const plaid = require('plaid');
 const envvar = require('envvar');
 const express = require('express');
 const bodyParser = require('body-parser');
 const moment = require('moment');
+const next = require('next');
 const util = require('util');
 
+// env vars
 const APP_PORT = envvar.number('APP_PORT', 8000);
+const DEV = envvar.string('NODE_ENV') !== 'production';
 const PLAID_CLIENT_ID = envvar.string('PLAID_CLIENT_ID');
 const PLAID_SECRET = envvar.string('PLAID_SECRET');
 const PLAID_PUBLIC_KEY = envvar.string('PLAID_PUBLIC_KEY');
@@ -25,104 +29,103 @@ const client = new plaid.Client(
 let ACCESS_TOKEN = null;
 let ITEM_ID = null;
 
-const app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: false,
-}));
-app.use(bodyParser.json());
+const app = next({dev: DEV});
+const handle = app.getRequestHandler()
 
-const prettyPrintResponse = (response) => {
-  console.log(util.inspect(response, { colors: true, depth: 4 }));
-};
+app.prepare().then(() => {
+  const server = express();
 
-
-app.get('/', (request, response) => {
-  response.render('index.ejs', {
-    PLAID_PUBLIC_KEY,
-    PLAID_ENV,
-    PLAID_PRODUCTS,
-    PLAID_COUNTRY_CODES,
-  });
-});
-
-app.post('/get_access_token', (request, response) => {
-  const PUBLIC_TOKEN = request.body.public_token;
-  client.exchangePublicToken(PUBLIC_TOKEN, (error, tokenResponse) => {
-    if (error != null) {
-      prettyPrintResponse(error);
-      return response.json({
-        error,
+  server.use(bodyParser.urlencoded({
+    extended: false,
+  }));
+  server.use(bodyParser.json());
+  
+  const prettyPrintResponse = (response) => {
+    console.log(util.inspect(response, { colors: true, depth: 4 }));
+  };
+  
+  server.post('/get_access_token', (req, res) => {
+    const PUBLIC_TOKEN = req.body.public_token;
+    client.exchangePublicToken(PUBLIC_TOKEN, (error, tokenResponse) => {
+      if (error != null) {
+        prettyPrintResponse(error);
+        return res.json({
+          error,
+        });
+      }
+      ACCESS_TOKEN = tokenResponse.access_token;
+      ITEM_ID = tokenResponse.item_id;
+      prettyPrintResponse(tokenResponse);
+      return res.json({
+        access_token: ACCESS_TOKEN,
+        item_id: ITEM_ID,
+        error: null,
       });
-    }
-    ACCESS_TOKEN = tokenResponse.access_token;
-    ITEM_ID = tokenResponse.item_id;
-    prettyPrintResponse(tokenResponse);
-    return response.json({
-      access_token: ACCESS_TOKEN,
-      item_id: ITEM_ID,
-      error: null,
     });
   });
-});
-
-// Retrieve real-time Balances for each of an Item's accounts
-// https://plaid.com/docs/#balance
-app.get('/balance', (request, response) => {
-  client.getBalance(ACCESS_TOKEN, (error, balanceResponse) => {
-    if (error != null) {
-      prettyPrintResponse(error);
-      return response.json({
-        error,
-      });
-    }
-    prettyPrintResponse(balanceResponse);
-    return response.json({ error: null, balance: balanceResponse });
+  
+  // Retrieve real-time Balances for each of an Item's accounts
+  // https://plaid.com/docs/#balance
+  server.get('/balance', (req, res) => {
+    client.getBalance(ACCESS_TOKEN, (error, balanceResponse) => {
+      if (error != null) {
+        prettyPrintResponse(error);
+        return res.json({
+          error,
+        });
+      }
+      prettyPrintResponse(balanceResponse);
+      return res.json({ error: null, balance: balanceResponse });
+    });
   });
-});
-
-// Retrieve Transactions for an Item
-// https://plaid.com/docs/#transactions
-app.get('/transactions', (request, response) => {
-  // Pull transactions for the Item for the last 30 days
-  const startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
-  const endDate = moment().format('YYYY-MM-DD');
-  client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
-    count: 250,
-    offset: 0,
-  }, (error, transactionsResponse) => {
-    const answer = {};
-
-    if (error != null) {
-      prettyPrintResponse(error);
-      answer.error = error;
-    } else {
-      prettyPrintResponse(transactionsResponse);
-      answer.error = null;
-      answer.transactions = transactionsResponse;
-    }
-
-    return response.json(answer);
+  
+  // Retrieve Transactions for an Item
+  // https://plaid.com/docs/#transactions
+  server.get('/transactions', (req, res) => {
+    // Pull transactions for the Item for the last 30 days
+    const startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+    const endDate = moment().format('YYYY-MM-DD');
+    client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+      count: 250,
+      offset: 0,
+    }, (error, transactionsResponse) => {
+      const answer = {};
+  
+      if (error != null) {
+        prettyPrintResponse(error);
+        answer.error = error;
+      } else {
+        prettyPrintResponse(transactionsResponse);
+        answer.error = null;
+        answer.transactions = transactionsResponse;
+      }
+  
+      return res.json(answer);
+    });
   });
-});
-
-// Retrieve an Item's accounts
-// https://plaid.com/docs/#accounts
-app.get('/accounts', (request, response) => {
-  client.getAccounts(ACCESS_TOKEN, (error, accountsResponse) => {
-    if (error != null) {
-      prettyPrintResponse(error);
-      return response.json({
-        error,
-      });
-    }
-    prettyPrintResponse(accountsResponse);
-    return response.json({ error: null, accounts: accountsResponse });
+  
+  // Retrieve an Item's accounts
+  // https://plaid.com/docs/#accounts
+  server.get('/accounts', (req, res) => {
+    client.getAccounts(ACCESS_TOKEN, (error, accountsResponse) => {
+      if (error != null) {
+        prettyPrintResponse(error);
+        return res.json({
+          error,
+        });
+      }
+      prettyPrintResponse(accountsResponse);
+      return res.json({ error: null, accounts: accountsResponse });
+    });
   });
-});
+  
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  })
 
+  server.listen(APP_PORT, err => {
+    if (err) throw err;
+    console.log(`> server listening on port ${APP_PORT}`);
+  });
+})
 
-app.listen(APP_PORT, () => {
-  console.log(`server listening on port ${APP_PORT}`);
-});
