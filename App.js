fad7de690778589ee501b66038d00e7d6a0812c2
Jon Corbin <jon.corbin8@gmail.com>
2020-01-20 01:35:27 -0500

    some quick eslint exceptions

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    // eslint-disable-next-line no-use-before-define
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
