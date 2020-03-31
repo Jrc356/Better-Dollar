febf63573298cd2f0891a59a40bbd0b4db0f8ca6
Jon Corbin <jon.corbin8@gmail.com>
2020-03-30 20:33:31 -0400

    convert cb to promises

--- server/server.js
+++ server/server.js
@@ -19,13 +19,15 @@ const PLAID_ENV = envvar.string('PLAID_ENV', 'sandbox');
 const PLAID_PRODUCTS = envvar.string('PLAID_PRODUCTS', 'transactions');
 const PLAID_COUNTRY_CODES = envvar.string('PLAID_COUNTRY_CODES', 'US');
 
-const client = new plaid.Client(
+// Create plaid client
+const plaidClient = new plaid.Client(
   PLAID_CLIENT_ID,
   PLAID_SECRET,
   PLAID_PUBLIC_KEY,
   plaid.environments[PLAID_ENV],
 );
 
+// For plaid
 let ACCESS_TOKEN = null;
 let ITEM_ID = null;
 
@@ -44,6 +46,7 @@ app.prepare().then(() => {
   }));
   server.use(bodyParser.json());
 
+  // Retrieve an access token
   server.post('/get_access_token', (req, res) => {
     console.log('> Received public token request');
     console.log('> headers:');
@@ -53,38 +56,32 @@ app.prepare().then(() => {
     const PUBLIC_TOKEN = req.body.public_token;
 
     console.log('Retrieving access token...');
-    client.exchangePublicToken(PUBLIC_TOKEN, (error, tokenResponse) => {
-      if (error != null) {
-        prettyPrintResponse(error);
+    plaidClient.exchangePublicToken(PUBLIC_TOKEN)
+      .then((tokenResponse) => {
+        ACCESS_TOKEN = tokenResponse.access_token;
+        ITEM_ID = tokenResponse.item_id;
+        console.log('> Token retreived');
+        prettyPrintResponse(tokenResponse);
         return res.json({
-          error,
+          access_token: ACCESS_TOKEN,
+          item_id: ITEM_ID,
+          error: null,
         });
-      }
-      ACCESS_TOKEN = tokenResponse.access_token;
-      ITEM_ID = tokenResponse.item_id;
-      console.log('> Token retreived');
-      prettyPrintResponse(tokenResponse);
-      return res.json({
-        access_token: ACCESS_TOKEN,
-        item_id: ITEM_ID,
-        error: null,
-      });
-    });
+      })
+      .catch((err) => res.json({
+        err,
+      }));
   });
 
   // Retrieve real-time Balances for each of an Item's accounts
   // https://plaid.com/docs/#balance
   server.get('/balance', (req, res) => {
-    client.getBalance(ACCESS_TOKEN, (error, balanceResponse) => {
-      if (error != null) {
-        prettyPrintResponse(error);
-        return res.json({
-          error,
-        });
-      }
-      prettyPrintResponse(balanceResponse);
-      return res.json({ error: null, balance: balanceResponse });
-    });
+    plaidClient.getBalance(ACCESS_TOKEN)
+      .then((balanceResponse) => {
+        prettyPrintResponse(balanceResponse);
+        return res.json({ error: null, balance: balanceResponse });
+      })
+      .catch((err) => res.json({ err }));
   });
 
   // Retrieve Transactions for an Item
@@ -93,43 +90,34 @@ app.prepare().then(() => {
     // Pull transactions for the Item for the last 30 days
     const startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
     const endDate = moment().format('YYYY-MM-DD');
-    client.getTransactions(ACCESS_TOKEN, startDate, endDate, {
+    plaidClient.getTransactions(ACCESS_TOKEN, startDate, endDate, {
       count: 250,
       offset: 0,
-    }, (error, transactionsResponse) => {
-      const answer = {};
-
-      if (error != null) {
-        prettyPrintResponse(error);
-        answer.error = error;
-      } else {
+    })
+      .then((transactionsResponse) => {
+        const answer = {};
         prettyPrintResponse(transactionsResponse);
         answer.error = null;
         answer.transactions = transactionsResponse;
-      }
-
-      return res.json(answer);
-    });
+        return res.json(answer);
+      })
+      .catch((err) => res.json({ err }));
   });
 
   // Retrieve an Item's accounts
   // https://plaid.com/docs/#accounts
   server.get('/accounts', (req, res) => {
-    client.getAccounts(ACCESS_TOKEN, (error, accountsResponse) => {
-      if (error != null) {
-        prettyPrintResponse(error);
-        return res.json({
-          error,
-        });
-      }
-      prettyPrintResponse(accountsResponse);
-      return res.json({ error: null, accounts: accountsResponse });
-    });
+    plaidClient.getAccounts(ACCESS_TOKEN)
+      .then((accountsResponse) => {
+        prettyPrintResponse(accountsResponse);
+        return res.json({ error: null, accounts: accountsResponse });
+      })
+      .catch((err) => res.json({ err }));
   });
 
-  server.all('*', (req, res) => { return handle(req, res); });
+  server.all('*', (req, res) => handle(req, res));
 
-  server.listen(APP_PORT, err => {
+  server.listen(APP_PORT, (err) => {
     if (err) throw err;
     console.log(`> server listening on port ${APP_PORT}`);
   });
